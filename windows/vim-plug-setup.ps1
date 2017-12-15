41593e012b31017d2505436de9a2139163a8f3fc
Julien Kauffmann <julien.kauffmann@ubisoft.com>
2017-12-15 15:29:10 -0500

    Fixed vim folder on Windows

--- windows/vim-plug-setup.ps1
+++ windows/vim-plug-setup.ps1
@@ -16,6 +16,6 @@ $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 (New-Object Net.WebClient).DownloadFile(
   $uri,
   $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
-    "~\vim\autoload\plug.vim"
+    "~\vimfiles\autoload\plug.vim"
   )
 )
