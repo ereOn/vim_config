50636783ea5e33cdc9be039e25e8ecb98068de1d
Julien Kauffmann <julien.kauffmann@freelan.org>
2022-09-29 11:44:45 -0400

    New keybindings

--- lua/user/keymappings.lua
+++ lua/user/keymappings.lua
@@ -1,10 +1,10 @@
 -- Fuzzy search
-vim.keymap.set("n", "<C-p>", require("telescope.builtin").find_files, {})
-vim.keymap.set("n", "<C-b>", require("telescope.builtin").buffers, {})
-vim.keymap.set("n", "<C-l>", require("telescope.builtin").treesitter, {})
+vim.keymap.set("n", "<leader>p", require("telescope.builtin").find_files, {})
+vim.keymap.set("n", "<leader>b", require("telescope.builtin").buffers, {})
+vim.keymap.set("n", "<leader>l", require("telescope.builtin").treesitter, {})
 
 -- Neotree
-vim.keymap.set("n", "<C-left>", "<cmd>Neotree toggle<cr>", {})
+vim.keymap.set("n", "<leader>t", "<cmd>Neotree toggle<cr>", {})
 
 -- Trouble
 vim.keymap.set("n", "<C-down>", "<cmd>Trouble<cr>", {})
