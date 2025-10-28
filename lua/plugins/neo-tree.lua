44109ddbc923ccbb55760dc95d7ee60cb36fd62c
Julien Kauffmann <julien.kauffmann@freelan.org>
2025-10-28 13:55:16 -0400

    Added follow the current file in neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,5 +8,12 @@ return {
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 			"MunifTanjim/nui.nvim",
 		},
+		opts = {
+			filesystem = {
+				bind_to_cwd = true,
+				follow_current_file = { enabled = true },
+				use_libuv_file_watcher = true,
+			},
+		},
 	},
 }
